5cfe017b31fc27cd62acf8f36c03290b922a33f3
Christian Llupo <jebediah47@protonmail.com>
2025-02-10 19:13:01 +0200

    Update 20250209201824_InitialCreate.cs

--- Migrations/20250209201824_InitialCreate.cs
+++ Migrations/20250209201824_InitialCreate.cs
@@ -15,23 +15,39 @@ namespace GalacticaBotAPI.Migrations
                 columns: table => new
                 {
                     UserId = table.Column<string>(type: "text", nullable: false),
-                    Username = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
-                    Email = table.Column<string>(type: "character varying(320)", maxLength: 320, nullable: true),
-                    AvatarHash = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: true),
+                    Username = table.Column<string>(
+                        type: "character varying(32)",
+                        maxLength: 32,
+                        nullable: true
+                    ),
+                    Email = table.Column<string>(
+                        type: "character varying(320)",
+                        maxLength: 320,
+                        nullable: true
+                    ),
+                    AvatarHash = table.Column<string>(
+                        type: "character varying(32)",
+                        maxLength: 32,
+                        nullable: true
+                    ),
                     Role = table.Column<int>(type: "integer", nullable: false),
-                    IsProfileComplete = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                    IsProfileComplete = table.Column<bool>(
+                        type: "boolean",
+                        nullable: false,
+                        defaultValue: false
+                    ),
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_AdminUsers", x => x.UserId);
-                });
+                }
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "AdminUsers");
+            migrationBuilder.DropTable(name: "AdminUsers");
         }
     }
 }
